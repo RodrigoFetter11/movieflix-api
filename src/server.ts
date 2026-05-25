c8a98bfa464d33bd7b6358de64fe46f2eea336e1
Rodrigo Fetter <rfetter182182@gmail.com>
2026-05-24 23:43:46 -0300

    adicionada a mensagem de erro na criação do filme

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
+app.use(express.json());
+
 
 app.get("/movies", async (_, res) => {
     const movies = await prisma.movie.findMany({
@@ -18,6 +20,25 @@ app.get("/movies", async (_, res) => {
     });
     res.json(movies);
 });
+
+app.post('/movies', async (req, res) => {
+    const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+ 
+ try{  
+    await prisma.movie.create({
+    data: {
+     title: title,
+     genre_id: genre_id,
+     language_id: language_id,
+     oscar_count: oscar_count,
+     release_date: new Date(release_date),
+   },
+ });
+    res.status(201).send();
+    } catch (error) {
+        return res.status(500).json({ message: "Erro ao criar o filme" });
+    }
+});  
 
 app.listen(port, () => {
      console.log(`Servidor em execução na porta ${port}`);
